86bbcc6e8fa8110bea7a3f3d74a5c68c5bce903a
EmilyAlberty <emily.alberty@upr.edu>
2025-09-09 14:40:56 +0000

    esquema y formulas

--- main.cpp
+++ main.cpp
@@ -1,1 +1,20 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
 
+int main(){
+
+    srand(time(0));
+
+    int max = 100;
+    int min = 0;
+
+    int numeroAleatorio1 = rand() % (max - min + 1) + min;
+    int numeroAleatorio2 = rand() % (max - min + 1) + min;
+    int numeroAleatorio3 = rand() % (max - min + 1) + min;
+
+    cout << "Numeros aleatorios generados: " << numeroAleatorio1 << ", " << numeroAleatorio2 << ", " <<numeroAleatorio3 << endl;
+
+    cout << "Orden descendiente: " << endl; 
+
+}
